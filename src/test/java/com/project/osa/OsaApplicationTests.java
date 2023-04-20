0a2fd725892375829434a2057ed0aebdeaddad04
Khondekar <mustak-riyaj.khondekar@capgemini.com>
2023-04-20 19:18:18 +0530

    Initial commit

---
+++ src/test/java/com/project/osa/OsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.osa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
